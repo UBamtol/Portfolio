28859e1f2c698e064abe3c01f824c12d79e880fd
UBamtol <yij0325@gmail.com>
2025-03-09 20:28:21 +0900

    chore: Change directory structure
    디렉터리 구조 변경

---
+++ src/components/title.tsx
@@ -0,0 +1,5 @@
+const title = () => {
+  return <>title 테스트1</>;
+};
+
+export default title;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@/styles/index.css';
+// import App from './App.tsx'
+import Title from '@components/title.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <Title />
+  </StrictMode>
+);
