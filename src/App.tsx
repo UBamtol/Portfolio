a0747bcd5aa507e27295546445037c0f4c5f20ef
UBamtol <yij0325@gmail.com>
2025-03-21 21:07:37 +0900

    refactor(src): 컴포넌트 파일 import 위치 변경
    새로고침시 스크롤 위치를 페이지 최상단에 위치시키기 위해 main.tsx -> App.tsx로 컴포넌트들의 import 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import './App.css';
+import Home from '@/components/Home';
+import About from '@/components/About';
+import Skils from '@/components/Skils';
+import Projects from '@/components/Projects';
+import { useEffect } from 'react';
+import '@/styles/index.css';
 
 function App() {
-  return <></>;
+  useEffect(() => {
+    window.history.scrollRestoration = 'manual';
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <div className="min-h-screen">
+      <Home />
+      <About />
+      <Skils />
+      <Projects />
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@/styles/index.css';
-import Home from '@/components/Home';
-import About from './components/About';
-import Skils from './components/Skils';
+
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* 여기에 가 바탕이 됨 */}
-    <div className="min-h-screen">
-      <Home />
-      <About />
-      <Skils />
-    </div>
+    <App />
   </StrictMode>
 );
